acef2d866c31883c2e88c8815ae5c0fd513ac7a8
c0ffeeartc <c0ffeeartc@gmail.com>
2014-06-30 20:50:01 +0300

    keys remapping and hotkeys. Slow response times

--- actionizer/hotkeyManager/Hotkey.py
+++ actionizer/hotkeyManager/Hotkey.py
@@ -1,7 +1,8 @@
 from PySide.QtCore import QTimer
 from PySide.QtGui import QKeySequence
-# import pyHook
 from pyHook.HookManager import *
+import win32api
+import win32con
 from activeappinfo.activeappinfo import ActiveAppInfo
 import win32com.client
 from hotkeyManager.remaplist import RemapList
@@ -28,6 +29,28 @@ class Hotkey(object):
         self.is_listening_paused = False
         self.is_photoshop = False
 
+        self.is_holding_left_ctrl = False
+        self.is_holding_left_alt = False
+        self.is_holding_left_shift = False
+
+        self.is_holding_right_ctrl = False
+        self.is_holding_right_alt = False
+        self.is_holding_right_shift = False
+
+        self.is_left_ctrl_down_injected = False
+        self.is_left_ctrl_up_injected = False
+        self.is_left_alt_down_injected = False
+        self.is_left_alt_up_injected = False
+        self.is_left_shift_down_injected = False
+        self.is_left_shift_up_injected = False
+
+        self.is_right_ctrl_down_injected = False
+        self.is_right_ctrl_up_injected = False
+        self.is_right_alt_down_injected = False
+        self.is_right_alt_up_injected = False
+        self.is_right_shift_down_injected = False
+        self.is_right_shift_up_injected = False
+
         self.timer = QTimer()
         # noinspection PyUnresolvedReferences
         self.timer.timeout.connect(self.process_key_events)
@@ -46,45 +69,143 @@ class Hotkey(object):
                 print("Hotkey: " + key_seq)
                 self.hotkey_list[key_seq].play()
 
+    # fixme: slows down response from keyboard
+    # fixme: sometimes program leaves injected keydowns on exit, and breaks keyboard input
     def on_key(self, event):
         """
         @type event: pyHook.HookManager.KeyboardEvent
         @return:
         """
         if self.is_listening and not self.is_listening_paused:
-            # print 'MessageName:', event.MessageName
-            # print 'Message:', event.Message
-            # print 'Time:', event.Time
-            # print 'Window:', event.Window
-            # print 'WindowName:', event.WindowName
-            # print 'Ascii:', event.Ascii, chr(event.Ascii)
-            # print 'Key:', event.Key
-            # print 'KeyID:', event.KeyID
-            # print 'ScanCode:', event.ScanCode
-            # print 'Extended:', event.Extended
-            # print 'Injected:', event.Injected
-            # print 'Alt', event.Alt
-            # print 'Transition', event.Transition
-            # print '---'
+            if event.IsInjected():
+                print '--- Injected Key:', event.Key, '- MessageName:', event.MessageName
+                self.set_injected_flags(event)
+                return True
+            # self.print_event(event)
 
-            is_ctrl = bool(GetKeyState(HookConstants.VKeyToID("VK_CONTROL")))
-            is_alt = bool(GetKeyState(HookConstants.VKeyToID("VK_MENU")))
-            is_shift = bool(GetKeyState(HookConstants.VKeyToID("VK_SHIFT")))
+            self.update_hold_flags(event)
+            self.fix_modifiers()
 
             key_seq = ""
-            if is_ctrl:
+            if self.is_holding_left_ctrl or self.is_holding_right_ctrl:
                 key_seq += "Ctrl+"
-            if is_alt:
+            if self.is_holding_left_alt or self.is_holding_right_alt:
                 key_seq += "Alt+"
-            if is_shift:
+            if self.is_holding_left_shift or self.is_holding_right_shift:
                 key_seq += "Shift+"
+
             key_seq += QKeySequence(event.Key).toString()
             if key_seq in self.remap.remap_list.keys():
                 return self.add_key_seq_to_que(key_seq, event)
 
-            elif not event.IsInjected() and key_seq in self.hotkey_list.keys():
+            elif key_seq in self.hotkey_list.keys():
                 return self.add_key_seq_to_que(key_seq, event)
         return True
+
+    def print_event(self, event):
+        print 'MessageName:', event.MessageName
+        print 'Message:', event.Message
+        print 'Time:', event.Time
+        print 'Window:', event.Window
+        print 'WindowName:', event.WindowName
+        print 'Ascii:', event.Ascii, chr(event.Ascii)
+        print 'Key:', event.Key
+        print 'KeyID:', event.KeyID
+        print 'ScanCode:', event.ScanCode
+        print 'Extended:', event.Extended
+        print 'Injected:', event.Injected
+        print 'Alt', event.Alt
+        print 'Transition', event.Transition
+        print '---'
+
+    def update_hold_flags(self, event):
+        if event.Message == HookConstants.WM_KEYDOWN or event.Message == HookConstants.WM_SYSKEYDOWN:
+            if event.KeyID == win32con.VK_LMENU:
+                self.is_holding_left_alt = True
+
+            elif event.KeyID == win32con.VK_LCONTROL:
+                self.is_holding_left_ctrl = True
+                
+            elif event.KeyID == win32con.VK_LSHIFT:
+                self.is_holding_left_shift = True
+
+            elif event.KeyID == win32con.VK_RMENU:
+                self.is_holding_right_alt = True
+
+            elif event.KeyID == win32con.VK_RCONTROL:
+                self.is_holding_right_ctrl = True
+
+            elif event.KeyID == win32con.VK_RSHIFT:
+                self.is_holding_right_shift = True
+
+        elif event.Message == HookConstants.WM_KEYUP or event.Message == HookConstants.WM_SYSKEYUP:
+            if event.KeyID == win32con.VK_LMENU:
+                self.is_holding_left_alt = False
+
+            elif event.KeyID == win32con.VK_LCONTROL:
+                self.is_holding_left_ctrl = False
+
+            elif event.KeyID == win32con.VK_LSHIFT:
+                self.is_holding_left_shift = False
+
+            elif event.KeyID == win32con.VK_RMENU:
+                self.is_holding_right_alt = False
+
+            elif event.KeyID == win32con.VK_RCONTROL:
+                self.is_holding_right_ctrl = False
+
+            elif event.KeyID == win32con.VK_RSHIFT:
+                self.is_holding_right_shift = False
+
+    def set_injected_flags(self, event):
+        if event.Message == HookConstants.WM_KEYUP or event.Message == HookConstants.WM_SYSKEYUP:
+            if event.KeyID == win32con.VK_LMENU:
+                self.is_left_alt_up_injected = True
+            elif event.KeyID == win32con.VK_LCONTROL:
+                self.is_left_ctrl_up_injected = True
+            elif event.KeyID == win32con.VK_LSHIFT:
+                self.is_left_shift_up_injected = True
+            elif event.KeyID == win32con.VK_RMENU:
+                self.is_right_alt_up_injected = True
+            elif event.KeyID == win32con.VK_RCONTROL:
+                self.is_right_ctrl_up_injected = True
+            elif event.KeyID == win32con.VK_RSHIFT:
+                self.is_right_shift_up_injected = True
+
+        elif event.Message == HookConstants.WM_KEYDOWN or event.Message == HookConstants.WM_SYSKEYDOWN:
+            if event.KeyID == win32con.VK_LMENU:
+                self.is_left_alt_down_injected = True
+            elif event.KeyID == win32con.VK_LCONTROL:
+                self.is_left_ctrl_down_injected = True
+            elif event.KeyID == win32con.VK_LSHIFT:
+                self.is_left_shift_down_injected = True
+            elif event.KeyID == win32con.VK_RMENU:
+                self.is_right_alt_down_injected = True
+            elif event.KeyID == win32con.VK_RCONTROL:
+                self.is_right_ctrl_down_injected = True
+            elif event.KeyID == win32con.VK_RSHIFT:
+                self.is_right_shift_down_injected = True
+
+    def fix_modifiers(self):
+        self.fix_vk(win32con.VK_LMENU, self.is_holding_left_alt, self.set_is_left_alt_down_injected, self.set_is_left_alt_up_injected)
+        self.fix_vk(win32con.VK_LCONTROL, self.is_holding_left_ctrl, self.set_is_left_ctrl_down_injected, self.set_is_left_ctrl_up_injected)
+        self.fix_vk(win32con.VK_LSHIFT, self.is_holding_left_shift, self.set_is_left_shift_down_injected, self.set_is_left_shift_up_injected)
+
+        self.fix_vk(win32con.VK_RMENU, self.is_holding_right_alt, self.set_is_right_alt_down_injected, self.set_is_right_alt_up_injected)
+        self.fix_vk(win32con.VK_RCONTROL, self.is_holding_right_ctrl, self.set_is_right_ctrl_down_injected, self.set_is_right_ctrl_up_injected)
+        self.fix_vk(win32con.VK_RSHIFT, self.is_holding_right_shift, self.set_is_right_shift_down_injected, self.set_is_right_shift_up_injected)
+
+    def fix_vk(self, vk_id, flag_holding, setter_injected_down, setter_injected_up):
+        if flag_holding and setter_injected_up():
+            # print "injecting " + HookConstants.id_to_vk[vk_id]
+            win32api.keybd_event(vk_id, 0, win32con.KEYEVENTF_EXTENDEDKEY | 0, 0)
+            setter_injected_down(True)
+            setter_injected_up(False)
+
+        elif setter_injected_down() and not flag_holding:
+            # print "fixing " + HookConstants.id_to_vk[vk_id]
+            win32api.keybd_event(vk_id, 0, win32con.KEYEVENTF_EXTENDEDKEY | win32con.KEYEVENTF_KEYUP, 0)
+            setter_injected_down(False)
 
     def add_key_seq_to_que(self, key_seq, event):
         if event.Message == HookConstants.WM_KEYDOWN or \
@@ -124,3 +245,67 @@ class Hotkey(object):
             key_seq_formatted += RemapList.SHIFT
         key_seq_formatted += buttons[-1].upper()
         self.shell.sendKeys(key_seq_formatted)
+
+    # -----------------------------------------
+    # Setters
+    # -----------------------------------------
+    def set_is_left_alt_down_injected(self, value=None):
+        if value is None:
+            return self.is_left_alt_down_injected
+        self.is_left_alt_down_injected = value
+
+    def set_is_right_alt_down_injected(self, value=None):
+        if value is None:
+            return self.is_right_alt_down_injected
+        self.is_right_alt_down_injected = value
+
+    def set_is_left_ctrl_down_injected(self, value=None):
+        if value is None:
+            return self.is_left_ctrl_down_injected
+        self.is_left_ctrl_down_injected = value
+
+    def set_is_right_ctrl_down_injected(self, value=None):
+        if value is None:
+            return self.is_right_ctrl_down_injected
+        self.is_right_ctrl_down_injected = value
+
+    def set_is_left_shift_down_injected(self, value=None):
+        if value is None:
+            return self.is_left_shift_down_injected
+        self.is_left_shift_down_injected = value
+
+    def set_is_right_shift_down_injected(self, value=None):
+        if value is None:
+            return self.is_right_shift_down_injected
+        self.is_right_shift_down_injected = value
+
+    def set_is_left_alt_up_injected(self, value=None):
+        if value is None:
+            return self.is_left_alt_up_injected
+        self.is_left_alt_up_injected = value
+
+    def set_is_right_alt_up_injected(self, value=None):
+        if value is None:
+            return self.is_right_alt_up_injected
+        self.is_right_alt_up_injected = value
+
+    def set_is_left_ctrl_up_injected(self, value=None):
+        if value is None:
+            return self.is_left_ctrl_up_injected
+        self.is_left_ctrl_up_injected = value
+
+    def set_is_right_ctrl_up_injected(self, value=None):
+        if value is None:
+            return self.is_right_ctrl_up_injected
+        self.is_right_ctrl_up_injected = value
+
+    def set_is_left_shift_up_injected(self, value=None):
+        if value is None:
+            return self.is_left_shift_up_injected
+        self.is_left_shift_up_injected = value
+
+    def set_is_right_shift_up_injected(self, value=None):
+        if value is None:
+            return self.is_right_shift_up_injected
+        self.is_right_shift_up_injected = value
+    # -------------------------------------
--- actionizer/hotkeyManager/remaplist.py
+++ actionizer/hotkeyManager/remaplist.py
@@ -8,10 +8,14 @@ class RemapList(object):
 
     def __init__(self):
         self.remap_list = {}
-        self.remap_list["Alt+1"] = "%{[}"
+        self.remap_list["Alt+1"] = "%["
+        self.remap_list["Alt+2"] = "%]"
+        self.remap_list["Alt+Shift+1"] = "%+["
+        self.remap_list["Alt+Shift+2"] = "%+]"
         self.remap_list["A"] = "%{[}"
         self.remap_list["S"] = "%{]}"
-        self.remap_list["Alt+2"] = "%{]}"
+        self.remap_list["Ctrl+1"] = "^["
+        self.remap_list["Ctrl+2"] = "^]"
 
     def add_remap(self, from_key, to_key):
         self.remove_remap(from_key)
